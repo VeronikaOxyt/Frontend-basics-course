a476c9708faaa3e04718ac59818ae9e861955653
Ivan Serebryakov <ivan.yu.serebryakov@rosbank.ru>
2023-05-06 19:34:08 +0300

    home-work tests

---
+++ __tests__/ex10_html-css-fundamentals-part2.test.js
@@ -0,0 +1,13 @@
+const {task, css, html} = require('~utils')('ex10_html-css-fundamentals-part2', true, 'html');
+
+describe('Ex10. HTML & CSS Fundamentals. Part 2', () => {
+    const tasks = Array.from({length: 4}).map((_, i) => `0${i + 1}`);
+    tasks.push('03-1', '03-2')
+
+    tasks.forEach(t =>
+        task(t, markup => {
+            it('html should be valid', html(markup));
+            it('css should be valid', css(markup));
+        }),
+    );
+});
---
+++ __tests__/ex3_js-objects-part1.test.js
@@ -0,0 +1,62 @@
+const {task} = require('~utils')('ex3_js-objects-part1');
+
+describe('Ex3. JS Objects Part 1', () => {
+    task('02', code => {
+        beforeAll(() => spyOn(console, 'log'));
+
+        const obj = {a: 1};
+
+        it("should be called once for 'obj' object", () => {
+            code(obj);
+            expect(console.log.calls.count()).toBe(1);
+        });
+    });
+
+    task('03', code => {
+        const obj = {a: 1};
+
+        it("should return 'true' if key exists in object", () =>
+            expect(code('a', obj)).toBe(true));
+
+        it("should return 'false' if key not exists in object", () =>
+            expect(code('b', obj)).toBe(false));
+    });
+
+    task('04', code => {
+        const obj = {a: 1};
+
+        it('should leave property unchanged if it exists', () => {
+            code('a', obj);
+            expect(obj.a).toBe(1);
+        });
+
+        it("should set 'new' to property if it doesn't exist", () => {
+            code('b', obj);
+            expect(obj.b).toBe('new');
+        });
+    });
+
+    task('05', code => {
+        const obj = {a: 1};
+
+        it('should create object with own new reference', () => {
+            expect(obj === code(obj)).toBeFalsy();
+        });
+    });
+
+    task('06', code => {
+        const obj = {a: 1, b: {c: 0}, e: {f: [{g: 1}]}};
+        const objCopy = code(obj);
+
+        it('should create object with new references for non-primitive values', () => {
+            expect(obj === objCopy).toBeFalsy();
+            expect(obj.b === objCopy.b).toBeFalsy();
+            expect(obj.e === objCopy.e).toBeFalsy();
+            expect(obj.e.f === objCopy.e.f).toBeFalsy();
+        });
+
+        it('should copy all properties', () => {
+            expect(objCopy).toEqual(obj);
+        });
+    });
+});
