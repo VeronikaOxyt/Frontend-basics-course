93ad4b7e84193c293417297a7bca24bc61ae1c97
Veronika Menshina <nikkimensh76@gmail.com>
2023-05-10 16:18:19 +0300

    ex5

---
+++ src/ex2_js-basics/task-05.js
@@ -0,0 +1,11 @@
+function maximum(array) {
+    let maxValue = 0;
+   array.forEach(value => {
+       if (value > maxValue) {
+           maxValue = value;
+       }
+   })
+   return maxValue;
+}
+
+module.exports = maximum
