19e6ee53aeb235177bbe9053a8301fcf9b088a6a
Veronika Menshina <nikkimensh76@gmail.com>
2023-05-10 16:16:29 +0300

    ex1

---
+++ src/ex2_js-basics/task-01.js
@@ -0,0 +1,12 @@
+function getType(value) {
+    if (typeof(value) === 'number' && !Number.isNaN(value)) {
+        return 'number';
+    }
+    
+    if (typeof(value) === 'string') {
+        return 'string';
+    } 
+    return undefined;
+}
+
+module.exports = getType
