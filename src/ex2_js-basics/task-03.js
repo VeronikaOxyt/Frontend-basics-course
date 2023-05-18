267cfb2ec13401f0f0dfabc0a300a20fc2d6d6d2
Veronika Menshina <nikkimensh76@gmail.com>
2023-05-18 19:53:48 +0300

    exesise 3

--- src/ex2_js-basics/task-03.js
+++ src/ex2_js-basics/task-03.js
@@ -1,19 +1,18 @@
 function EvenUneven(array) {
     let EvenElem = 0;
     let UnevenElem = 0;
-    let nullElem = 0;
+    let zeroCounter = 0;
     array.forEach(value => {
         if (typeof(value) === 'number') {
-            if(value !== 0 && value % 2 === 0) {
+            if(value === 0) {
+                zeroCounter++; }
+            else if(value % 2 === 0) {
                 EvenElem++;
             } else if(value % 2 === 1) {
-                UnevenElem++;
-            } else if(value === 0) {
-                nullElem++; }
+                UnevenElem++; }  
         }
     })
-    let list = [EvenElem, UnevenElem, nullElem];
-    return list;
+    return [EvenElem, UnevenElem, zeroCounter];
 }
 
 module.exports = EvenUneven
