9303f84a1fecffcdeb59f78d4face65ab1448a21
Veronika Menshina <nikkimensh76@gmail.com>
2023-08-02 15:38:35 +0300

    exesise 9

--- src/ex4_js-objects-part2/task-09.js
+++ src/ex4_js-objects-part2/task-09.js
@@ -1,9 +1,6 @@
 function pasteStr(str1, str2, n) {
     let arr = str1.split(' ');
-    let arrBox = arr.slice(0, n + 1);
-    arrBox.push(str2); 
-    arrBox = arrBox.concat(arr.slice(n + 1));
-    let string = arrBox.join(' ');
-return string;
+    arr.splice(n+1, 0, str2);
+    return arr.join(' ');
 }
 module.exports = pasteStr
